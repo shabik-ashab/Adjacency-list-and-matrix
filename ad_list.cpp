6e962d89a9d1994725c313046c3006e769ec4227
nub69 <shabik.ashab@gmail.com>
2024-02-25 20:37:08 +0600

    adjacency list added

--- ad_list.cpp
+++ ad_list.cpp
@@ -31,12 +31,20 @@ using namespace std;
 int main()
 {
     int nodes = 4;
+    vector<int>adj_list[nodes];
+
+    adj_list[0] = {1};
+    adj_list[1] = {0,2,3};
+    adj_list[2] = {1,3};
+    adj_list[3] = {1,2};
+
     
     for (int i = 0; i < nodes; i++)
     {
-        for (int j = 0; j < nodes; j++)
+        cout<<i<<"->";
+        for (int j = 0; j < adj_list[i].size(); j++)
         {
-            cout<<matrix[i][j]<<" ";
+            cout<<adj_list[i][j]<<" ";
         }
         cout<<endl;
     }
---
+++ edge_list.cpp
@@ -0,0 +1,9 @@
+/*
+      0 --2---1
+          /1  \2 
+         2--5--3 
+    it is impossible to understand if the graph is directed or not, we
+    need to inform
+    undirected graph edge list -> [0,1,2], [1,2,1], [1,3,2], [2,3,5]
+
+*/
